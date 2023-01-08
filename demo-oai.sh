74e715e6190fc957e146365397dd4fe80bb1803e
turletti <turletti@gmail.com>
2023-01-08 17:50:02 +0100

    fixed aw2s interface to net3

--- demo-oai.sh
+++ demo-oai.sh
@@ -216,7 +216,7 @@ EOF
 s|n2hostInterface:.*|n2hostInterface: "$GNB_IF_NAME_N2"|
 s|n3hostInterface:.*|n3hostInterface: "$GNB_IF_NAME_N3"|
 s|aw2shostInterface:.*|aw2shostInterface: "$LOC_IF_NAME_AW2S"|
-s|localIfName:.*|localIfName: "$LOC_IF_NAME_AW2S"|
+s|localIfName:.*|localIfName: "$net3"|
 s|remoteAddr.*|remoteAddr: "$LOC_ADDR_AW2S"|
 s|localAddr.*|localAddr: "$ADDR_AW2S"|
 s|nodeName:.*|nodeName: $node_gnb|
@@ -324,7 +324,7 @@ EOF
     else
 	SED_FILE="/tmp/aw2s_conf.sed"
 	cat > "$SED_FILE" <<EOF
-s|local_if_name.*|local_if_name  = "$LOC_IF_NAME_AW2S"|
+s|local_if_name.*|local_if_name  = "net3"|
 s|remote_address.*|remote_address = "$LOC_ADDR_AW2S"|
 s|local_address.*|local_address = "$ADDR_AW2S"|
 EOF
