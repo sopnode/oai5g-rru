eaeb59b135d80d5efd88103e9707979dbf185fd0
turletti <turletti@gmail.com>
2023-01-20 18:10:35 +0100

    minor fix

--- demo-oai.sh
+++ demo-oai.sh
@@ -568,7 +568,7 @@ function get-cn-pcap(){
 
     AMF_POD_NAME=$(kubectl get pods --namespace $ns -l "app.kubernetes.io/name=oai-amf,app.kubernetes.io/instance=oai-amf" -o jsonpath="{.items[0].metadata.name}")
     echo "Retrieve OAI5G cn pcap files from oai-amf pod, ns $ns"
-    kubectl -c tcpdump -n $ns exec -it $AMF_POD_NAME -- /bin/tar cfz cn-pcap.tgz pcap
+    kubectl -c tcpdump -n $ns exec -i $AMF_POD_NAME -- /bin/tar cfz cn-pcap.tgz pcap
     kubectl -c tcpdump cp $ns/$AMF_PODNAME:cn-pcap.tgz /tmp/cn-pcap.tgz
 }
 
@@ -578,7 +578,7 @@ function get-ran-pcap(){
 
     GNB_POD_NAME=$(kubectl get pods --namespace $ns -l "app.kubernetes.io/name=oai-gnb,app.kubernetes.io/instance=oai-gnb" -o jsonpath="{.items[0].metadata.name}")
     echo "Retrieve OAI5G ran pcap file from oai-gnb pod, ns $ns"
-    kubectl -c tcpdump -n $ns exec -it $GNB_POD_NAME -- /bin/tar cfz ran-pcap.tgz pcap
+    kubectl -c tcpdump -n $ns exec -i $GNB_POD_NAME -- /bin/tar cfz ran-pcap.tgz pcap
     kubectl -c tcpdump cp $ns/$GNB_PODNAME:ran-pcap.tgz /tmp/ran-pcap.tgz
 }
 
