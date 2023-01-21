f9a66c342a22c6de5c2d0b2271a014da6ce07bdb
turletti <turletti@gmail.com>
2023-01-21 21:37:07 +0100

    few fixes

--- demo-oai.sh
+++ demo-oai.sh
@@ -76,7 +76,7 @@ function get-all-logs() {
     ns=$1; shift
     prefix=$1; shift
 
-DATE=`date +"%Y-%m-%dT%H:%M:%S"`
+DATE=`date +"%Y-%m-%dT%H.%M.%S"`
 
 AMF_POD_NAME=$(kubectl get pods --namespace $ns -l "app.kubernetes.io/name=oai-amf,app.kubernetes.io/instance=oai-amf" -o jsonpath="{.items[0].metadata.name}")
 AMF_eth0_IP=$(kubectl get pods --namespace $ns -l "app.kubernetes.io/name=oai-amf,app.kubernetes.io/instance=oai-amf" -o jsonpath="{.items[*].status.podIP}")
@@ -125,13 +125,13 @@ function get-cn-pcap(){
     ns=$1; shift
     prefix=$1; shift
 
-    DATE=`date +"%Y-%m-%dT%H:%M:%S"`
+    DATE=`date +"%Y-%m-%dT%H.%M.%S"`
 
     AMF_POD_NAME=$(kubectl get pods --namespace $ns -l "app.kubernetes.io/name=oai-amf,app.kubernetes.io/instance=oai-amf" -o jsonpath="{.items[0].metadata.name}")
     echo "Retrieve OAI5G cn pcap files from oai-amf pod, ns $ns"
     echo "kubectl -c tcpdump -n $ns exec -i $AMF_POD_NAME -- /bin/tar cfz cn-pcap.tgz pcap"
     kubectl -c tcpdump -n $ns exec -i $AMF_POD_NAME -- /bin/tar cfz cn-pcap.tgz pcap
-    echo "kubectl -c tcpdump cp $ns/$AMF_PODNAME:cn-pcap.tgz $prefix/cn-pcap.tgz"
+    echo "kubectl -c tcpdump cp $ns/$AMF_POD_NAME:cn-pcap.tgz $prefix/cn-pcap.tgz"
     kubectl -c tcpdump cp $ns/$AMF_POD_NAME:cn-pcap.tgz $prefix/cn-pcap-"$DATE".tgz
 }
 
@@ -140,17 +140,17 @@ function get-ran-pcap(){
     ns=$1; shift
     prefix=$1; shift
 
-    DATE=`date +"%Y-%m-%dT%H:%M:%S"`
+    DATE=`date +"%Y-%m-%dT%H.%M.%S"`
 
     GNB_POD_NAME=$(kubectl get pods --namespace $ns -l "app.kubernetes.io/name=oai-gnb,app.kubernetes.io/instance=oai-gnb" -o jsonpath="{.items[0].metadata.name}")
     echo "Retrieve OAI5G ran pcap files from oai-gnb pod, ns $ns"
     echo "kubectl -c tcpdump -n $ns exec -i $GNB_POD_NAME -- /bin/tar cfz ran-net1-pcap.tgz pcap"
     kubectl -c tcpdump -n $ns exec -i $GNB_POD_NAME -- /bin/tar cfz ran-net1-pcap.tgz pcap
-    echo "kubectl -c tcpdump cp $ns/$GNB_PODNAME:ran-net1-pcap.tgz $prefix/ran-net1-pcap-"$DATE".tgz"
+    echo "kubectl -c tcpdump cp $ns/$GNB_POD_NAME:ran-net1-pcap.tgz $prefix/ran-net1-pcap-"$DATE".tgz"
     kubectl -c tcpdump cp $ns/$GNB_POD_NAME:ran-net1-pcap.tgz $prefix/ran-net1-pcap-"$DATE".tgz
     echo "kubectl -c tcpdump2 -n $ns exec -i $GNB_POD_NAME -- /bin/tar cfz ran-net2-pcap.tgz pcap"
     kubectl -c tcpdump -n $ns exec -i $GNB_POD_NAME -- /bin/tar cfz ran-net2-pcap.tgz pcap
-    echo "kubectl -c tcpdump cp $ns/$GNB_PODNAME:ran-net2-pcap.tgz $prefix/ran-net2-pcap-"$DATE".tgz"
+    echo "kubectl -c tcpdump cp $ns/$GNB_POD_NAME:ran-net2-pcap.tgz $prefix/ran-net2-pcap-"$DATE".tgz"
     kubectl -c tcpdump cp $ns/$GNB_POD_NAME:ran-net2-pcap.tgz $prefix/ran-net2-pcap-"$DATE".tgz
 }
 
