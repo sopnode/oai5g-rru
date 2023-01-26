0910595cb8336ce7fdceffc40f160bde3c64ef69
turletti <turletti@gmail.com>
2023-01-26 09:48:47 +0100

    debug

--- demo-oai.sh
+++ demo-oai.sh
@@ -385,7 +385,7 @@ EOF
 	cat >> "$SED_FILE" <<EOF
 s|sfp1hostInterface:.*|sfp1hostInterface: "$IF_NAME_LOCAL_N3XX_1"|
 s|sfp2hostInterface:.*|sfp2hostInterface: "$IF_NAME_LOCAL_N3XX_2"|
-s|useAdditionalOptions:.*|useAdditionalOptions: "--sa --usrp-tx-thread-config 1 --tune-offset 30000000 --thread-pool 1,3,5,7,9,11,13,15 --log_config.global_log_options level,nocolor,time|
+s|useAdditionalOptions:.*|useAdditionalOptions: "--sa --usrp-tx-thread-config 1 --tune-offset 30000000 --thread-pool 1,3,5,7,9,11,13,15 --log_config.global_log_options level,nocolor,time"|
 EOF
     elif [[ "$rru" == "jaguar" || "$rru" == "panther" ]]; then
 	if [ "$rru" == "jaguar" ] ; then
