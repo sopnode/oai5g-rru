f90df8212f9f0c025a174832c9624c1bdcedaa09
turletti <turletti@gmail.com>
2023-02-13 19:47:20 +0100

    change rfsim conf file

--- demo-oai.sh
+++ demo-oai.sh
@@ -51,7 +51,9 @@ CONF_JAGUAR="jaguar_panther2x2_50MHz.conf"
 CONF_PANTHER="panther4x4_20MHz.conf"
 CONF_B210="gnb.sa.band78.fr1.24PRB.usrpb210.conf"
 CONF_N3XX="gnb.band78.sa.fr1.106PRB.2x2.usrpn310.conf"
-CONF_RFSIM="gnb.sa.band78.106prb.rfsim.2x2.conf"
+#CONF_RFSIM="gnb.sa.band78.106prb.rfsim.2x2.conf"
+CONF_RFSIM="gnb.sa.band78.fr1.24PRB.usrpb210.conf"
+
 
 OAI5G_CHARTS="$HOME"/oai-cn5g-fed/charts
 OAI5G_CORE="$OAI5G_CHARTS"/oai-5g-core
