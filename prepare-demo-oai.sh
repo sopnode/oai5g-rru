26adcc3fb18fb6ef1a6af791250656e88092f848
turletti <turletti@gmail.com>
2023-05-14 15:33:43 +0200

    debug

--- prepare-demo-oai.sh
+++ prepare-demo-oai.sh
@@ -24,9 +24,12 @@ git clone -b $TAG_OAI5G_RRU $REPO_OAI5G_RRU
 echo "git clone -b $TAG_OAI_CN5G_FED $REPO_OAI_CN5G_FED"
 git clone -b $TAG_OAI_CN5G_FED $REPO_OAI_CN5G_FED
 cp oai5g-rru/configure-demo-oai.sh .
-echo "./configure-demo-oai.sh update $NS $HOST_AMF_SPGWU $HOST_GNB $RRU $GNB_ONLY $PCAP $DIR $RC_NAME $RC_PWD $RC_MAIL"
-./configure-demo-oai.sh update $NS $HOST_AMF_SPGWU $HOST_GNB $RRU $GNB_ONLY $PCAP $DIR $RC_NAME $RC_PWD $RC_MAIL
 cp oai5g-rru/demo-oai.sh .
 chmod a+x demo-oai.sh
+echo "./configure-demo-oai.sh update $NS $HOST_AMF_SPGWU $HOST_GNB $RRU $GNB_ONLY $PCAP $DIR $RC_NAME $RC_PWD $RC_MAIL"
+./configure-demo-oai.sh update $NS $HOST_AMF_SPGWU $HOST_GNB $RRU $GNB_ONLY $PCAP $DIR $RC_NAME $RC_PWD $RC_MAIL
+echo "run init"
+./demo-oai.sh init $NS
 echo "./demo-oai.sh configure-all $HOST_AMF_SPGWU $HOST_GNB $RRU $PCAP"
 ./demo-oai.sh configure-all $HOST_AMF_SPGWU $HOST_GNB $RRU $PCAP 
+
