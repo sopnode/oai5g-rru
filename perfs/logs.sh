72ded1ef2e405d065397e514e62bb710462d98b6
turletti <turletti@gmail.com>
2023-06-01 22:35:48 +0200

    add options to logs.sh script

--- perfs/logs.sh
+++ perfs/logs.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
 ns="oai5g" # Default namespace
+nf="gnb" #Default OAI5G network function
 
 usage()
 {
-   echo "Usage: $0 oai-function"
-   echo -e "\twhere oai-function in [amf, gnb, nr-ue]"
-   exit 1
+   echo "Usage: $0 [-n namespace] [-f oai-function]"
+   echo -e "\twith oai-function in {amf, gnb, nr-ue}"
+   exit 1 
 }
 
-if [ $# -ne 1 ]; then
-    usage
-else
-    oaiLabel="$1"
-    if [[ ($oaiLabel != "amf") && ($oaiLabel != "gnb") && ($oaiLabel != "nr-ue") ]]; then
-       usage
-    fi
-fi
-
-
-echo "Wait until oai-${oaiLabel} pod is Ready..."
-while [[ $(kubectl -n $ns get pods -l app.kubernetes.io/name=oai-"${oaiLabel}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
-   sleep 1
+while getopts 'n:f:' flag; do
+  case "${flag}" in
+    n) ns="${OPTARG}" ;;
+    f) nf="${OPTARG}" ;;
+    *) usage
+       exit 1 ;;
+  esac
 done
 
-# Retrieve the pod name
-POD_NAME=$(kubectl -n$ns get pods -l app.kubernetes.io/name=oai-"${oaiLabel}" -o jsonpath="{.items[0].metadata.name}")
+if [[ ($nf != "amf") && ($nf != "gnb") && ($nf != "nr-ue") ]]; then
+    usage
+fi
 
-echo "Show logs of "oai-${oaiLabel} pod $POD_NAME
-kubectl -n "$ns" -c "${oaiLabel}" logs -f $POD_NAME
+echo "$0: Showing oai-${nf} pod logs on ${ns} namespace"
+
+while true; do
+    echo "Wait until oai-${nf} pod is Ready..." 
+    while [[ $(kubectl -n $ns get pods -l app.kubernetes.io/name=oai-"${nf}" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+	sleep 1
+    done
+
+    # Retrieve the pod name
+    POD_NAME=$(kubectl -n$ns get pods -l app.kubernetes.io/name=oai-"${nf}" -o jsonpath="{.items[0].metadata.name}")
+
+    echo "Show logs of "oai-${nf} pod $POD_NAME
+    kubectl -n "$ns" -c "${nf}" logs -f $POD_NAME
+done
