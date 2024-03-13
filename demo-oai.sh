9e3d4b10ae7c6ffc0e680b4f6c0b70943043c50c
turletti <turletti@gmail.com>
2024-03-13 22:33:34 +0100

    modify UPF/gNB/NR-UE IP addresses to match 192.168.128.144/28 subnet

--- demo-oai.sh
+++ demo-oai.sh
@@ -46,7 +46,7 @@ PREFIX_STATS="/tmp/oai5g-stats"
 OAISA_REPO="docker.io/oaisoftwarealliance"
 P100="192.168.100"
 SUBNET_N2N3="192.168.128"
-NETMASK_N2N3="27"
+NETMASK_N2N3="28"
 IF_NAME_N2N3="eth2"
 
 
