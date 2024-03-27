4044f4a9e7e72551290e02c107ea706f72901d49
turletti <turletti@gmail.com>
2024-03-27 16:41:06 +0100

    git merge all dnn changes and fixes from develop-r2lab-tmp branch

--- demo-oai.py
+++ demo-oai.py
@@ -34,12 +34,12 @@ from r2lab import r2lab_hostname, ListOfChoices, ListOfChoicesNullReset, find_lo
 ##########################################################################################
 #    Configure here OAI5G_RRU and OAI_CN5G_FED repo and tag
 OAI5G_RRU_REPO = 'https://github.com/sopnode/oai5g-rru.git'
-OAI5G_RRU_TAG = 'develop-r2lab-tmp'
+OAI5G_RRU_TAG = 'develop-r2lab'
 #OAI5G_RRU_TAG = 'r2-helm-charts'
 #OAI5G_RRU_TAG = 'master'
 #OAI5G_RRU_TAG = 'v1.5.1-1.3-1.0'
 OAI_CN5G_FED_REPO = 'https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed.git'
-OAI_CN5G_FED_TAG = 'develop-r2lab-tmp'
+OAI_CN5G_FED_TAG = 'develop-r2lab'
 #OAI_CN5G_FED_TAG = 'r2-helm-charts-r2lab'
 #OAI_CN5G_FED_TAG = 'v1.5.1-1.3'
 ##########################################################################################
