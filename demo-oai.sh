d1a28f886b86872cd4dc59133b76922f764717ba
turletti <turletti@gmail.com>
2024-04-05 19:15:12 +0200

    update

--- demo-oai.sh
+++ demo-oai.sh
@@ -983,7 +983,7 @@ function start-gnb() {
 	echo "helm -n $NS install oai-cu oai-cu/"
 	helm -n $NS install oai-cu oai-cu/
 
-	echo "sleep 10s"; sleep 10
+	echo "sleep 10s"; sleep 30
 	echo "kubectl -n $NS wait pod --for=condition=Ready -l app.kubernetes.io/instance=oai-cu"
 	kubectl -n $NS wait pod --for=condition=Ready -l app.kubernetes.io/instance=oai-cu
 	echo "helm install -n $NS oai-du oai-du/"
