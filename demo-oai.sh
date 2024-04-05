190b48c1d6734b39060559d517962c780cf8d25e
turletti <turletti@gmail.com>
2024-04-05 19:13:09 +0200

    update

--- demo-oai.sh
+++ demo-oai.sh
@@ -210,7 +210,7 @@ HOST_AMF="oai-amf"
 F1IFNAME="f1"
 E1IFNAME="e1"
 F1CUPORT="2152"
-F1DUPORT="2152"
+F1DUPORT="2153"
 #
 ########## DU specific part ##############
 #DU_REPO="${R2LAB_REPO}/oai-gnb" DU_REPO must be GNB_REPO to handle aw2s case
