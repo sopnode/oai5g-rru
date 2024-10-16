e420ed186c2e2766f52cd6542efd9fb9e8d3eb8e
turletti <turletti@users.noreply.github.com>
2024-10-16 14:23:27 +0200

    add configuration for qhat07 UE with 2 DNN

--- configure-demo-oai.sh
+++ configure-demo-oai.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 ##########################################################################################
-#    Configure here the following variables used in demo-oai.sh script
+#  Configure here the following variables used in demo-oai.sh script and in MYSQL database
 #
-MCC="001" # default is "208"
-MNC="01" # default is "95"
-TAC="1" # default is "1"
-#DNN0 and DNN1 must be set in demo-oai.py or prepare-demo-oai.sh to configure Quectel UE
+MCC="001" 
+MNC="01" 
+TAC="1" 
+
+# DNN0 and DNN1 must be set in demo-oai.py or in prepare-demo-oai.sh scripts to configure Quectel UE
+#  if DNN1=="none", configure a single DNN will be configured in the mysql database
+DNN0_PDU_TYPE="IPV4" # "IPV4" or "IPV4V6"
+DNN1_PDU_TYPE="IPV4" # "IPV4" or "IPV4V6"
+
 SLICE1_SST="1"
-#SLICE1_SD="000002"
 #
 # NOTA on SD format encoding in the charts
 # - in mysql database, SD field is a string/hexadecimal without 0x prefix,
@@ -17,17 +21,28 @@ SLICE1_SST="1"
 # - in core/config.yaml and ue/values.yaml, it is in hex form without 0x prefix
 SLICE1_SD="ABCDEF"
 SLICE1_5QI="5" # non-GBR
+SLICE1_ARP_PRIORITY_LEVEL="15"
+SLICE1_ARP_PREEMPT_CAP="NOT_PREEMPT" # "NOT_PREEMPT" or "MAY_PREEMPT" # to trigger preemption
+SLICE1_ARP_PREEMPT_VULN="PREEMPTABLE" # "PREEMPTABLE" or "NOT_PREEMPT" # preemption vulnerability
+SLICE1_PRIORITY_LEVEL="1"
 SLICE1_UPLINK="20Mbps"
 SLICE1_DOWNLINK="40Mbps"
+
 SLICE2_SST="1"
 SLICE2_SD="FFFFFF"
 SLICE2_5QI="6" # non-GBR
+SLICE2_ARP_PRIORITY_LEVEL="15"
+SLICE2_ARP_PREEMPT_CAP="NOT_PREEMPT" # "NOT_PREEMPT" or "MAY_PREEMPT" # to trigger preemption
+SLICE2_ARP_PREEMPT_VULN="PREEMPTABLE" # "PREEMPTABLE" or "NOT_PREEMPT" # preemption vulnerability
+SLICE2_PRIORITY_LEVEL="1"
 SLICE2_UPLINK="100Mbps"
 SLICE2_DOWNLINK="200Mbps"
+
 GNB_ID="0xe020"
 FULL_KEY="fec86ba6eb707ed08905757b1bb44b8f" # default is "8baf473f2f8fd09487cccbd7097c6862"
 OPC="C42449363BBAD02B66D16BC975D77CC1" # default is "8E27B6AF0E692E750F32667A3B14605D"
-RFSIM_IMSI="001010000001121" # default is "208950000001121"
+RFSIM_IMSI="001010000001121"
+
 ##########################################################################################
 
 function update() {
@@ -74,15 +89,25 @@ s|@DEF_MCC@|${MCC}|g
 s|@DEF_MNC@|${MNC}|g
 s|@DEF_TAC@|${TAC}|g
 s|@DEF_DNN0@|${DNN0}|
+s|@DEF_DNN0_PDU_TYPE@|${DNN0_PDU_TYPE}|
 s|@DEF_DNN1@|${DNN1}|
+s|@DEF_DNN1_PDU_TYPE@|${DNN1_PDU_TYPE}|
 s|@DEF_SLICE1_SST@|${SLICE1_SST}|
 s|@DEF_SLICE1_SD@|${SLICE1_SD}|
 s|@DEF_SLICE1_5QI@|${SLICE1_5QI}|
+s|@DEF_SLICE1_ARP_PRIORITY_LEVEL@|${SLICE1_ARP_PRIORITY_LEVEL}|
+s|@DEF_SLICE1_ARP_PREEMPT_CAP@|${SLICE1_ARP_PREEMPT_CAP}|
+s|@DEF_SLICE1_ARP_PREEMPT_VULN@|${SLICE1_ARP_PREEMPT_VULN}|
+s|@DEF_SLICE1_PRIORITY_LEVEL@|${SLICE1_PRIORITY_LEVEL}|
 s|@DEF_SLICE1_UPLINK@|${SLICE1_UPLINK}|
 s|@DEF_SLICE1_DOWNLINK@|${SLICE1_DOWNLINK}|
 s|@DEF_SLICE2_SST@|${SLICE2_SST}|
 s|@DEF_SLICE2_SD@|${SLICE2_SD}|
 s|@DEF_SLICE2_5QI@|${SLICE2_5QI}|
+s|@DEF_SLICE2_ARP_PRIORITY_LEVEL@|${SLICE2_ARP_PRIORITY_LEVEL}|
+s|@DEF_SLICE2_ARP_PREEMPT_CAP@|${SLICE2_ARP_PREEMPT_CAP}|
+s|@DEF_SLICE2_ARP_PREEMPT_VULN@|${SLICE2_ARP_PREEMPT_VULN}|
+s|@DEF_SLICE2_PRIORITY_LEVEL@|${SLICE2_PRIORITY_LEVEL}|
 s|@DEF_SLICE2_UPLINK@|${SLICE2_UPLINK}|
 s|@DEF_SLICE2_DOWNLINK@|${SLICE2_DOWNLINK}|
 s|@DEF_GNB_ID@|${GNB_ID}|
@@ -103,10 +128,16 @@ EOF
     diff /tmp/demo-oai-orig.sh $PREFIX_DEMO/demo-oai.sh
 
     DIR_GENERIC_DB="$PREFIX_DEMO/oai5g-rru/patch-mysql"
-    cp $DIR_GENERIC_DB/oai_db-basic-generic.sql /tmp/
-    echo "Patching oai_db-basic.sql generic database with input parameters"
-    sed -f /tmp/demo-oai.sed < /tmp/oai_db-basic-generic.sql > $DIR_GENERIC_DB/oai_db-basic.sql
-    diff $DIR_GENERIC_DB/oai_db-basic-generic.sql $DIR_GENERIC_DB/oai_db-basic.sql
+    if [[ "$DNN1" = "none" ]]; then
+	echo "Patching oai_db-basic.sql generic database for R2lab UEs with DNN0 only"
+	oai_db-basic-template="oai_db-basic-generic.sql"
+    else
+	echo "Patching oai_db-basic.sql generic database for R2lab UEs with both DNN0 and DNN1"
+	oai_db-basic-template="oai_db-basic-generic-2dnn.sql"
+    fi
+    cp $DIR_GENERIC_DB/${oai_db-basic-template} /tmp/${oai_db-basic-template}
+    sed -f /tmp/demo-oai.sed < /tmp/${oai_db-basic-template} > $DIR_GENERIC_DB/oai_db-basic.sql
+    diff $DIR_GENERIC_DB/${oai_db-basic-template} $DIR_GENERIC_DB/oai_db-basic.sql
 
 }
 
