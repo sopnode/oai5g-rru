944bd6ee6d25c3cb8493ea57ee19faad8a5317e3
turletti <turletti@gmail.com>
2024-10-24 14:08:52 +0200

    add running options to prepare-demo.sh script

--- testing/prepare-demo-oai.sh
+++ testing/prepare-demo-oai.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 #
-# Usage: prepare-demo-oai.sh [TAG_OAI5G_RRU]
-#
-# This script aims to test oai5g-rru demo script without fit nodes,
-# by running all the pods on sopnode servers and simulating RAN with rfsim
+# This script aims to test the oai5g-rru demo script directly on a sopnode server
+# see https://github.com/sopnode/oai5g-rru/tree/develop-r2lab
 #
 
-#!/bin/bash
-
-DIR="/home/github-runner/actions-runner/_work/test-oai5g-rru"
-#DIR="/root/test-oai5g-rru"
+DIR="$(pwd)"
 REPO_OAI5G_RRU="https://github.com/sopnode/oai5g-rru.git"
-#TAG_OAI5G_RRU="v2.0.1-1.0-1.0"
-#TAG_OAI5G_RRU="master"
-if [ $# -eq 1 ]
-then
-    TAG_OAI5G_RRU="$1"
-else
-    TAG_OAI5G_RRU="develop-r2lab"
-fi
-echo "***** Testing demo oai5g-rru, TAG: $1"
+#TAG_OAI5G_RRU="2024.w31"
+TAG_OAI5G_RRU="develop-r2lab-2dnn"
 REPO_OAI_CN5G_FED="https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed.git"
-TAG_OAI_CN5G_FED="develop-r2lab"
-NS="oaiw1-ci"
+#TAG_OAI_CN5G_FED="develop-r2lab"
+TAG_OAI_CN5G_FED="develop-r2lab-2dnn"
+NS="oaiw1"
 HOST_AMF_UPF="sopnode-w1"
 HOST_GNB="sopnode-w1"
 #HOST_GNB="fit02"
@@ -30,37 +19,78 @@ HOST_GNB="sopnode-w1"
 #RRU="rfsim"
 #RRU="b210"
 RRU="jaguar"
-#RRU="n300"
+#RRU="n320"
 RUN_MODE="full"
+#RUN_MODE="gnb-upf"
 #RUN_MODE="gnb-only"
-#CN_MODE="advance"
-CN_MODE="basic"
-GNB_MODE="monolithic"
-#GNB_MODE="cudu"
+CN_MODE="advance"
+#CN_MODE="basic"
+#GNB_MODE="monolithic"
+GNB_MODE="cudu"
 #GNB_MODE="cucpup"
 DNN0="oai"
-DNN1="none"
+#DNN1="none"
+DNN1="internet"
 LOGS="true"
-PCAP="false"
+#PCAP="false"
+PCAP="true"
 RC_NAME="r2labuser"
 RC_PWD="r2labuser-pwd"
 RC_MAIL="r2labuser@turletti.com"
 
-echo "$0: Clean up previous oai5g-rru and oai-cn5g-fed.git local directories if any"
-cd $DIR
-rm -rf oai5g-rru oai-cn5g-fed
-echo "$0: Clone oai5g-rru and oai-cn5g-fed.git and configure charts and scripts"
-echo "git clone -b $TAG_OAI5G_RRU $REPO_OAI5G_RRU"
-git clone -b $TAG_OAI5G_RRU $REPO_OAI5G_RRU
-echo "git clone -b $TAG_OAI_CN5G_FED $REPO_OAI_CN5G_FED"
-git clone -b $TAG_OAI_CN5G_FED $REPO_OAI_CN5G_FED
-cp oai5g-rru/configure-demo-oai.sh .
-cp oai5g-rru/demo-oai.sh .
-chmod a+x demo-oai.sh
-echo "./configure-demo-oai.sh update $NS $HOST_AMF_UPF $HOST_GNB $RRU $RUN_MODE $LOGS $PCAP $DIR $CN_MODE $GNB_MODE $DNN0 $DNN1 $RC_NAME $RC_PWD $RC_MAIL"
-./configure-demo-oai.sh update $NS $HOST_AMF_UPF $HOST_GNB $RRU $RUN_MODE $LOGS $PCAP $DIR $CN_MODE $GNB_MODE $DNN0 $DNN1 $RC_NAME $RC_PWD $RC_MAIL
-echo "run init"
-./demo-oai.sh init
-echo "./demo-oai.sh configure-all"
-./demo-oai.sh configure-all
-echo "configure-demo-oai.sh update, demo-oai.sh init and demo-oai.sh configure-all steps done !"
+
+COMMAND=$(basename $0)
+
+function usage() {
+    echo "USAGE:"
+    echo "$COMMAND all : git pull the latest code and configure the OAI5G charts for the target scenario"
+    echo "$COMMAND pull : git pull the latest code. If necessary, you can manually modify the scripts before running configure."
+    echo "$COMMAND configure : configure the OAI5G charts for the target scenario"
+    echo "                    configure must only be run after a fresh pull, i.e., 2 consecutive configure will fail."
+    exit 1
+}
+
+function git_pull(){
+
+    echo "Step 1: clean up previous oai5g-rru and oai-cn5g-fed.git local directories if any"
+    cd $DIR
+    rm -rf oai5g-rru oai-cn5g-fed
+    echo "$0: Clone oai5g-rru and oai-cn5g-fed.git and configure charts and scripts"
+    echo "git clone -b $TAG_OAI5G_RRU $REPO_OAI5G_RRU"
+    git clone -b $TAG_OAI5G_RRU $REPO_OAI5G_RRU
+    echo "git clone -b $TAG_OAI_CN5G_FED $REPO_OAI_CN5G_FED"
+    git clone -b $TAG_OAI_CN5G_FED $REPO_OAI_CN5G_FED
+    echo "Step 2: retrieve latest configure-demo-oai.sh and demo-oai.sh scripts"
+    cp oai5g-rru/configure-demo-oai.sh .
+    cp oai5g-rru/demo-oai.sh .
+    chmod a+x demo-oai.sh
+    echo "Pull done. If necessary, you can manually modify these 2 scripts before running $COMMAND configure."
+}
+
+
+function configure_all_scripts(){
+    echo "Step 1: use parameters from configure-demo-oai.sh to configure demo-oai.sh script"
+    echo "./configure-demo-oai.sh update $NS $HOST_AMF_UPF $HOST_GNB $RRU $RUN_MODE $LOGS $PCAP $DIR $CN_MODE $GNB_MODE $DNN0 $DNN1 $RC_NAME $RC_PWD $RC_MAIL"
+    ./configure-demo-oai.sh update $NS $HOST_AMF_UPF $HOST_GNB $RRU $RUN_MODE $LOGS $PCAP $DIR $CN_MODE $GNB_MODE $DNN0 $DNN1 $RC_NAME $RC_PWD $RC_MAIL
+    echo "Step 2: configure OAI5G charts to match the target scenario"
+    echo "run init"
+    ./demo-oai.sh init
+    echo "./demo-oai.sh configure-all"
+    ./demo-oai.sh configure-all
+    echo "OAI5G charts are now configured for your scenario, you can use the start.sh script to launch your scenario."
+}
+
+if test $# -ne 1
+        then usage
+fi
+if [[ $1 = 'all' ]]; then
+    git_pull
+    configure_all_scripts
+elif [[ $1 = 'pull' ]]; then
+    git_pull
+elif [[ $1 = 'configure' ]]; then
+    configure_all_scripts
+else
+    usage
+fi
+
