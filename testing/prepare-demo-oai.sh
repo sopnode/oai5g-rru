c988b797816ff477a4bab87e51414d89f6f17def
turletti <turletti@gmail.com>
2024-10-24 14:22:09 +0200

    add running options to prepare-demo.sh script

--- testing/prepare-demo-oai.sh
+++ testing/prepare-demo-oai.sh
@@ -4,49 +4,65 @@
 # see https://github.com/sopnode/oai5g-rru/tree/develop-r2lab
 #
 
-DIR="$(pwd)"
+# server used for following OAI5G functions
+HOST_AMF_UPF="sopnode-w1"
+HOST_GNB="sopnode-w1"
+
+# k8s namespace
+NS="oaiw1"
+
+# Repo/Branch/TAG for code
 REPO_OAI5G_RRU="https://github.com/sopnode/oai5g-rru.git"
 #TAG_OAI5G_RRU="2024.w31"
 TAG_OAI5G_RRU="develop-r2lab-2dnn"
 REPO_OAI_CN5G_FED="https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed.git"
 #TAG_OAI_CN5G_FED="develop-r2lab"
 TAG_OAI_CN5G_FED="develop-r2lab-2dnn"
-NS="oaiw1"
-HOST_AMF_UPF="sopnode-w1"
-HOST_GNB="sopnode-w1"
+
+# CN mode
+CN_MODE="advance"
+#CN_MODE="basic"
+
+# oai5g-rru running mode 
+RUN_MODE="full"
+#RUN_MODE="gnb-upf"
+#RUN_MODE="gnb-only"
+
+# RAN options
+RRU="jaguar"
 #HOST_GNB="fit02"
 #HOST_GNB="up02"
 #RRU="rfsim"
 #RRU="b210"
-RRU="jaguar"
 #RRU="n320"
-RUN_MODE="full"
-#RUN_MODE="gnb-upf"
-#RUN_MODE="gnb-only"
-CN_MODE="advance"
-#CN_MODE="basic"
-#GNB_MODE="monolithic"
 GNB_MODE="cudu"
 #GNB_MODE="cucpup"
+#GNB_MODE="monolithic"
+
+# DNNs 
 DNN0="oai"
-#DNN1="none"
 DNN1="internet"
+#DNN1="none"
+
+# logs configuration
 LOGS="true"
-#PCAP="false"
-PCAP="true"
+PCAP="false"
+#PCAP="true"
+
+# identity used to git pull
 RC_NAME="r2labuser"
 RC_PWD="r2labuser-pwd"
 RC_MAIL="r2labuser@turletti.com"
 
 
+DIR="$(pwd)"
 COMMAND=$(basename $0)
 
 function usage() {
     echo "USAGE:"
-    echo "$COMMAND all : git pull the latest code and configure the OAI5G charts for the target scenario"
-    echo "$COMMAND pull : git pull the latest code. If necessary, you can manually modify the scripts before running configure."
-    echo "$COMMAND configure : configure the OAI5G charts for the target scenario"
-    echo "                    configure must only be run after a fresh pull, i.e., 2 consecutive configure will fail."
+    echo "$COMMAND all: git pull the latest code and configure the OAI5G charts for the target scenario"
+    echo "$COMMAND pull: git pull the latest code. If necessary, you can manually modify the scripts before running configure."
+    echo "$COMMAND configure: configure the OAI5G charts for the target scenario, configure must only be run after a fresh pull, i.e., 2 consecutive configure will fail."
     exit 1
 }
 
