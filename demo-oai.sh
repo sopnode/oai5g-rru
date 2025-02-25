261db0fe0de16ccb0c8bff5f01d15cea77169b2a
turletti <turletti@gmail.com>
2025-02-25 16:25:42 +0100

    add optional multus ovs for n3

--- demo-oai.sh
+++ demo-oai.sh
@@ -246,17 +246,18 @@ else
         # RUN_MODE=gnb-only
 	# -- Local RAN and external CN
         SUBNET_N2N3="172.21.10" # e.g., "10.0.20" # only used if multus on N2/N3
-        NETMASK_N2N3="26" # only used if multus on N2/N3
-        IF_NAME_N2N3="$IF_NAME_N2N3" # host interface used for multus on N2/N3 and also on e1/f1 network interfaces if not monolithic mode
+        NETMASK_N2N3="24" # only used if multus on N2/N3
+        IF_NAME_N2N3="n3br" #"$IF_NAME_N2N3" # host interface used for multus on N2/N3 and also on e1/f1 network interfaces if not monolithic mode
         # Set the external AMF IP address (N2)
-        HOST_AMF=${NODE_AMF_UPF%"-v100"} # open5gs-amf service is unknown, use $NODE_AMF_UPF to set up external IP address # XXX "$SUBNET_N2N3.201"
+        HOST_AMF="10.10.3.200" #${NODE_AMF_UPF%"-v100"} # open5gs-amf service is unknown, use $NODE_AMF_UPF to set up external IP address # XXX "$SUBNET_N2N3.201"
         # Set the local gNB host network interface to reach AMF/UPF (N2/N3)
 	MULTUS_GNB_N2="false" # XXX # only used if multus on N2/N3
 	IP_GNB_N2="$SUBNET_N2N3.223" # only used if multus on N2/N3
-	GNB_N2_IF_NAME="eth0" # pod network interface name for N2 (eth0 or n2)
+	GNB_N2_IF_NAME="n3" # pod network interface name for N2 (eth0 or n2)
         # Set the route to reach AMF/UPF
         ROUTES_GNB_N2="" # [{'dst': '172.22.10.0/24','gw': '10.0.20.1'}]"
-	MULTUS_GNB_N3="false"
+	MULTUS_GNB_N3="true"
+	IP_GNB_N3="10.10.3.205"
 	if [[ $GNB_MODE = 'cucpup' ]]; then
 	    IP_GNB_N3="$SUBNET_N2N3.224"
 	    IP_NRUE="$SUBNET_N2N3.225"
