912f79c23243b2e0fe0f5b59e257e37325b4dff0
turletti <turletti@users.noreply.github.com>
2025-02-25 22:46:00 +0100

    fix

--- demo-oai.sh
+++ demo-oai.sh
@@ -419,7 +419,7 @@ fi
 ########## GNB Monolithic specific part ################
 #
 NETMASK_GNB_N2="$NETMASK_N2N3"
-NETMASK_GNB_N3=""
+NETMASK_GNB_N3="$NETMASK_N2N3"
 NETMASK_GNB_RU="28"
 #
 ################## RRU-dependent part ###################
