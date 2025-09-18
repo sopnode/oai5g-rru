24a27df8fe6267b92a3f814e662a865b846d80b2
Thierry Turletti <turletti@users.noreply.github.com>
2025-09-18 15:37:01 +0200

    update the conf to support the new charts for benetel with branch fhi72-rlab

--- demo-oai.py
+++ demo-oai.py
@@ -37,7 +37,7 @@ OAI5G_RRU_REPO = 'https://github.com/sopnode/oai5g-rru.git'
 OAI5G_RRU_TAG = 'benetel' # XXX
 #OAI5G_RRU_TAG = 'v2.1.0-1.0-1.2'
 OAI_CN5G_FED_REPO = 'https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed.git'
-OAI_CN5G_FED_TAG = 'gen-cn2' # XXX
+OAI_CN5G_FED_TAG = 'fhi72-r2lab' # XXX
 #OAI_CN5G_FED_TAG = 'v2.1.0-1.2'
 ##########################################################################################
 
