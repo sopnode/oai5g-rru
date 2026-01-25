821c91333ee7dce4f202c6c475dc9dc7a54dd22e
turletti <turletti@users.noreply.github.com>
2026-01-25 18:12:00 +0100

    fixes for n3xx scenarios

--- demo-oai.sh
+++ demo-oai.sh
@@ -999,11 +999,11 @@ configure-gnb() {
     cp "${CONFIG_RRU}" "$CONFIG"
     ##diff -u <(yq eval -P '.' ${OAI5G_RAN}/${nf}/config.yaml.orig) <(yq eval -P '.' ${CONFIG})
 
-    # Update deployment.yaml and nad.yaml templates
-    cp -f "${NEW_TEMPLATES}/oai-gnb/nad.yaml" "${ORIG_GNB_TEMPLATES}"
-    cp -f "${NEW_TEMPLATES}/oai-du/nad.yaml" "${ORIG_DU_TEMPLATES}"
+    ## Update deployment.yaml and nad.yaml templates
+    #cp -f "${NEW_TEMPLATES}/oai-gnb/nad.yaml" "${ORIG_GNB_TEMPLATES}"
+    #cp -f "${NEW_TEMPLATES}/oai-du/nad.yaml" "${ORIG_DU_TEMPLATES}"
     # fix also this typo in the du deployment template 
-    sed -i 's/oai-cu.count/oai-du.count/g' "${ORIG_DU_TEMPLATES}/deployment.yaml"
+    #sed -i 's/oai-cu.count/oai-du.count/g' "${ORIG_DU_TEMPLATES}/deployment.yaml"
 
     # Fix deployment charts in the case of AW2S RUs as Eurecom no more support AW2S...
     if [[ "${RRU_TYPE}" == "aw2s" ]]; then
