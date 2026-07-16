1e2d8c5a3342127d497080772d2b9ca5c2fff321
gitlab-runner-r2lab <gitlab.runner@turletti.com>
2026-07-16 10:52:09 +0200

    update OAI5G_RRU, TAG_OAI5G_RRU and AW2S_GNB tags

--- demo-oai.sh
+++ demo-oai.sh
@@ -377,7 +377,7 @@ export CN_TAG="v2.2.1"
 OAI5G_RAN="${OAI5G_CHARTS}/oai-5g-ran"
 R2LAB_REPO="docker.io/r2labuser"
 
-export RAN_TAG="2026.w26" # "2025.w52"
+export RAN_TAG="2026.w28"
 
 # Default GNB REPO/TAG (can be overrided in rru/${rru}.env)
 export GNB_REPO="${R2LAB_REPO}/oai-gnb"
