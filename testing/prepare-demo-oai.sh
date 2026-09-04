319de4cb66b8d461f533389e965ef4c17f61bd64
gitlab-runner-r2lab <gitlab.runner@turletti.com>
2026-09-04 05:26:14 +0200

    update OAI5G_RRU, TAG_OAI5G_RRU and AW2S_GNB tags

--- testing/prepare-demo-oai.sh
+++ testing/prepare-demo-oai.sh
@@ -19,7 +19,7 @@ NS="oai-ci"
 
 # Repo/tag for oai5g-rru scripts
 REPO_OAI5G_RRU="https://github.com/sopnode/oai5g-rru.git"
-TAG_OAI5G_RRU="main"
+TAG_OAI5G_RRU="2026.w35"
 
 # Repo/tag for OAI charts
 REPO_OAI_CN5G_FED="https://gitlab.eurecom.fr/turletti/charts.git"
@@ -56,7 +56,7 @@ DNN1="streaming"
 # logs configuration
 #   -- logs and pcap are automatically retrieved when running demo-oai.sh stop
 #      in /tmp/tmp.root/oai5g-stats.tgz
-LOGS="false"
+LOGS="true"
 PCAP="false"
 MONITORING="false"
 FLEXRIC="false"
